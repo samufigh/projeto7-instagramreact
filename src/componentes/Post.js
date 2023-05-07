c3eaf5d6ce9ca9b44c43f9c87aa1dc8f2b64e06d
samufigh <samufigh21@gmail.com>
2023-05-07 17:03:03 -0300

    dados dinâmicos

--- src/componentes/Post.js
+++ src/componentes/Post.js
@@ -1,41 +1,91 @@
-export default function Post( props ){
-    return (
-        <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src={props.urlPerfil} alt={props.usuario}/>
-                {props.usuario}
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
+import React from "react";
+export default function Post(props) {
 
-            <div class="conteudo">
-              <img 
-              src={props.urlPost} 
-              alt={props.altPost}/>
-            </div>
+  const [coracao, setCoracao] = React.useState(false);
+  const coracaoAtual = coracao ? "heart" : "heart-outline";
+  const [corCoracao, setcorCoracao] = React.useState(false);
+  const corCoracaoAtual = corCoracao ? "heart-red" : "heart-black";
 
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
+  let [likes, setLikes] = React.useState(
+    Math.floor(Math.random() * 100000) + 100000
+  );
 
-              <div class="curtidas">
-                <img src="assets/perfis/sakura.jpg" alt="sakura"/>
-                <div class="texto">
-                  Curtido por <strong>sakura</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-              </div>
-            </div>
+  const [salvar, clickSalvar] = React.useState(false);
+  const salvarAtual = salvar ? "bookmark" : "bookmark-outline";
+
+
+  function likePost() {
+    setCoracao(!coracao);
+    setcorCoracao(!corCoracao);
+    if (!coracao) {
+      setLikes(likes + 1);
+    } else {
+      setLikes(likes - 1);
+    }
+  }
+
+  function likeByImg() {
+    if (coracaoAtual === "heart-outline") {
+      likePost();
+    }
+  }
+
+
+  return (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.urlPerfil} alt={props.usuario} />
+          {props.usuario}
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img
+          onClick={likeByImg}
+          src={props.urlPost}
+          alt={props.altPost} />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon
+              onClick={likePost}
+              name={coracaoAtual}
+              class={corCoracaoAtual}
+            >
+            </ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-    );
+          <div>
+            <ion-icon
+              onClick={() => clickSalvar(!salvar)}
+              name={salvarAtual}></ion-icon>
+          </div>
+        </div>
+
+        <div class="curtidas">
+          <img src="assets/perfis/sakura.jpg" alt="sakura" />
+          <div class="texto">
+            Curtido por <strong>nathanandrade</strong> e{" "}
+            <strong>
+              outras{" "}
+              <span data-test="likes-number">
+                {likes.toLocaleString("pt-BR", {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 2,
+                })}{" "}
+              </span>
+                 pessoas
+            </strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/componentes/User.js
+++ src/componentes/User.js
@@ -1,13 +1,44 @@
-export default function Usuario(){
-    return (
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-          <div class="texto">
-            <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
-            </span>
-          </div>
-        </div>
-    )
+import React from "react";
+export default function Usuario() {
+
+  const [username, attUsername] = React.useState("ninja");
+  const [perfil, attPerfil] = React.useState(
+    "assets/perfis/hinata.jpg"
+  );
+
+  function atualizarPerfil(){
+    let novoPerfil=prompt('imagem de perfil: ');
+    if(!novoPerfil){
+      return;
+    }
+    attPerfil(novoPerfil);
+  }
+
+  function atualizarusername() {
+    const novoUsername = prompt("username: ");
+    if (!novoUsername) {
+
+      return;
+    }
+    attUsername(novoUsername);
+  }
+
+  return (
+    <div class="usuario">
+      <img 
+      src={perfil} 
+      alt="imagem de perfil" 
+      onClick={atualizarPerfil}
+      />
+      <div class="texto">
+        <span>
+          <strong>{username}</strong>
+          <ion-icon
+            name="pencil"
+            onClick={atualizarusername}
+          ></ion-icon>
+        </span>
+      </div>
+    </div>
+  )
 }
